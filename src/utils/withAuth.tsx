73df5b9662bcf99f023d6ae4dd16fa0f40aa06c1
MD AMINUL ISLAM <mr.aminul118@gmail.com>
2025-08-16 14:45:13 +0600

    Fix withauth

--- src/utils/withAuth.tsx
+++ src/utils/withAuth.tsx
@@ -1,18 +1,17 @@
 import { useUserInfoQuery } from '@/redux/features/auth/auth.api';
 import type { TRole } from '@/types';
+import type { ComponentType } from 'react';
 import { Navigate } from 'react-router';
 
-export const withAuth = (Component: React.ComponentType, allowedRoles?: TRole[]) => {
+export const withAuth = (Component: ComponentType, allowedRoles?: TRole[]) => {
   return function AuthWrapper() {
     const { data, isLoading } = useUserInfoQuery(undefined);
     const user = data?.data;
 
     if (!isLoading && !user?.email) return <Navigate to="/login" />;
-
     if (!isLoading && allowedRoles && !allowedRoles.includes(user?.role as TRole)) {
       return <Navigate to="/unauthorized" />;
     }
-
     return <Component />;
   };
 };
